069c88a4dc337835f36f5ebb7fa64066df6722e1
Ibnu D. Aji <iaji@siskam.link>
2021-01-16 16:00:13 +0700

    refaktor skrip.

--- cari-koordinat.rkt
+++ cari-koordinat.rkt
@@ -3,27 +3,23 @@
 (define (hipotenusa-inggang x)
   (sqrt (+ (* x x) (* 19.05 19.05))))
 
-(define (derajat-tambahan stagger)
-  (atan (/ stagger 19.05)))
+(define (derajat-tambahan inggang)
+  (atan (/ inggang 19.05)))
 
-(define (koordinat-kiri x-sekarang y-sekarang inggang)
-  (let* ([hipotenusa (hipotenusa-inggang inggang)]
-         [delta (derajat-tambahan inggang)]
-         [derajat (+ (degrees->radians 30) delta)]
+(define (koordinat kiri? x-sekarang y-sekarang inggang)
+  (let* ([delta (derajat-tambahan inggang)]
+         [derajat-sisi (if kiri? 30 150)]
+         [derajat-radian (degrees->radians derajat-sisi)]
+         [derajat (+ derajat-radian (if kiri? delta (- delta)))]
+         [hipotenusa (hipotenusa-inggang inggang)]
          [x (* hipotenusa (cos derajat))]
          [y (* hipotenusa (sin derajat))]
          [x-baru (- x-sekarang x)]
          [y-baru (- y-sekarang y)])
     (list x-baru y-baru)))
 
-(define (koordinat-kanan x-sekarang y-sekarang inggang)
-  (let* ([hipotenusa (hipotenusa-inggang inggang)]
-         [delta (derajat-tambahan inggang)]
-         [derajat (- (degrees->radians 150) delta)]
-         [x (* hipotenusa (cos derajat))]
-         [y (* hipotenusa (sin derajat))]
-         [x-baru (- x-sekarang x)]
-         [y-baru (- y-sekarang y)])
-    (list x-baru y-baru)))
-
+(koordinat false 223.481 22.165 -15)
 (koordinat-kanan 223.481 22.165 -15)
+
+(koordinat true 223.481 22.165 -15)
+(koordinat-kiri 223.481 22.165 -15)
